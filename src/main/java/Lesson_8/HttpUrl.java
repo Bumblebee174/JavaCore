de4f28b6a00799ee63b77242ba16c5194a3b7253
Bumblebee174 <vvs.74rus@mail.ru>
2022-10-03 21:58:24 +0500

    Hw8

---
+++ src/main/java/Lesson_8/HttpUrl.java
@@ -0,0 +1,9 @@
+package Lesson_8;
+
+import java.nio.file.attribute.AclEntry;
+
+public interface HttpUrl {
+    static AclEntry parse(String s) {
+        return null;
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module com.example.javacore {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.annotation;
+    requires java.sql;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
 
 
     opens com.example.javacore to javafx.fxml;
